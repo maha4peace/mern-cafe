472711c1f67c88399f22ef408fbe6c3547425290
Maha <maha.ahmed4peace@gmail.com>
2024-02-20 02:30:46 -0500

    setting up the sign-up form

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,5 +1,10 @@
+import SignUpForm from "../components/SignUpForm/SignUpForm";
+
 export default function AuthPage(props) {
     return (
-        <h1>Auth Page </h1>
+        <main>
+            <h1>Auth Page </h1>
+            <SignUpForm/> 
+        </main>
     )
 }
